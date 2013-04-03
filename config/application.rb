ef44b0ffb62ba1f31c76194ed645eb26ff55e147
Cameron Leake <cameronleake7@gmail.com>
2013-04-03 22:22:44 +1030

    Server Rebuild Redeploy

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module ElephundRails
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
     
-    config.assets.initialize_on_precompile = false  # ADDED FOR DEPLOYMENT WITH CAPISTRANO
+    # config.assets.initialize_on_precompile = false  # ADDED FOR DEPLOYMENT WITH CAPISTRANO
   end
 end
