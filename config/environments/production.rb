ba451722341fdd7d35f193923dafe1c810457eea
Cameron Leake <cameronleake7@gmail.com>
2013-04-03 18:01:33 +1030

    Updating settings for CSS display #4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,7 @@ ElephundRails::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = true  # CHANGED FROM FALSE FOR DEPLOYMENT WITH CAPISTRANO
+  config.assets.compile = false  # CHANGED FROM FALSE FOR DEPLOYMENT WITH CAPISTRANO
 
   # Generate digests for assets URLs
   config.assets.digest = true
