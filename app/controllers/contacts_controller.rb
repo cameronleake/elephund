71554418d6257b0bd5c1014a3db31839ffc2221b
Cameron Leake <cameronleake7@gmail.com>
2013-04-11 15:00:59 +0930

    Delayed_Job - Alternative Approach #1

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,5 +1,12 @@
 class ContactsController < ApplicationController
   
+  class ContactFormJob < Struct.new(:contact)
+    def perform
+      ContactMailer.contact_us(contact).deliver
+    end
+  end
+  
+  
   def new
     @contact = Contact.new
   end
@@ -7,7 +14,7 @@ class ContactsController < ApplicationController
   def create
     @contact = Contact.new(params[:contact])
     if @contact.save
-      @contact.send_later(:send_contact_form)
+      Delayed::Job.enqueue ContactFormJob.new(@contact)
       # @contact.delay.send_contact_form
       redirect_to root_path, notice: "Form submitted!"
     else
